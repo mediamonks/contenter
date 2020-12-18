9c556ef2399d3696389d6683f8907b7db76dec86
Thijs <thijs@mediamonks.com>
2020-12-18 14:33:54 +0100

    fix type casting so we can remove the @ts-ignore

--- src/util.ts
+++ src/util.ts
@@ -14,8 +14,7 @@ function downloadFile(url: string, name: string) {
 async function copyValueToClipboard(value: string) {
   const result = await navigator
     .permissions
-    // @ts-ignore
-    .query({ name: 'clipboard-write' });
+    .query({ name: 'clipboard-write' as PermissionName });
 
   if (result.state !== 'granted' && result.state !== 'prompt') throw new Error('permission denied');
   await navigator.clipboard.writeText(value);
