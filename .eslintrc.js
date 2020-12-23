02c34d9b036c99033aaa56f2fa59fa7ad6636292
Thijs <thijs@mediamonks.com>
2020-12-23 16:34:31 +0100

    add default extends in .eslintrc.js because they are also needed for .ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['@mediamonks'],
+  extends: [
+    '@mediamonks',
+    'plugin:vue/vue3-recommended',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
+    'max-len': ['off'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/no-cycle': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
