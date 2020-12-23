de964c3b639f894d9ec0df57387741191022c651
Thijs <thijs@mediamonks.com>
2020-12-23 16:56:47 +0100

    disable rule object-curly-newline

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   rules: {
     'max-len': ['off'],
+    'object-curly-newline': ['off'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/no-cycle': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
